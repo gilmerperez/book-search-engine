79421683d4a651a8045262ff604f80d76e14ac59
gilmerperez <gilmer2002@outlook.com>
2025-03-03 21:18:16 -0500

    Added GET_ME query

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -3,6 +3,19 @@ import { gql } from '@apollo/client';
 // GET_ME will execute the 'me' query set up using Apollo Server
 export const GET_ME = gql`
   query me {
-    me {}
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
   }
 `;
